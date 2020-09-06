30266e3bdb63983995ddfe6b39ac55eb41a67248
Daniel Fuentes <fuentes.danav@gmail.com>
2020-09-06 01:17:16 -0500

    Added table view for track lists (album cover, song name, artists, album name)

--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -2,14 +2,39 @@ import React from "react"
 
 export default ({ trackList }) => {
   if (!trackList) return null
-  const TrackList = ({ trackList }) => {
+  const columns = ["cover", "name", "artists", "album"]
+  const TrackTable = ({ trackList }) => {
     return (
-      <ul>
+      <tbody>
+        <tr>
+          {columns.map((el, i) => (
+            <th key={-i}>{el}</th>
+          ))}
+        </tr>
+
         {trackList.map((elem, i) => (
-          <li key={i}>{elem.track.name}</li>
+          <tr key={i}>
+            <td>
+              {elem.track.album.images[2] && (
+                <img
+                  src={elem.track.album.images[2].url}
+                  alt="album cover"
+                  width={elem.track.album.images[2].width}
+                  height={elem.track.album.images[2].height}
+                ></img>
+              )}
+            </td>
+            <td>{elem.track.name}</td>
+            <td>{elem.track.artists.map(el => el.name).join(", ")}</td>
+            <td>{elem.track.album.name}</td>
+          </tr>
         ))}
-      </ul>
+      </tbody>
     )
   }
-  return <TrackList trackList={trackList} />
+  return (
+    <table>
+      <TrackTable trackList={trackList} />
+    </table>
+  )
 }
--- trackformat.js
+++
@@ -1,1 +0,0 @@
-[{added_at: "2020-08-29T00:25:09Z", track: {album: {album_type: "album",…},…}},…]
