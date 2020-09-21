84956a3c29f9d62adc3120d297d409da5f43546d
Daniel Fuentes <fuentes.danav@gmail.com>
2020-09-21 01:07:38 -0500

    Added nav-bar using styled components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react"
+import styled from "@emotion/styled"
 import {
   BrowserRouter as Router,
   Route,
@@ -7,29 +8,75 @@ import {
 } from "react-router-dom"
 import Home from "./components/Home"
 import About from "./components/About"
-import "./App.css"
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  flex-direction: column;
+`
+
+const TopNav = () => {
+  const Nav = styled.nav`
+    background: black;
+    font-size: 16px;
+    width: 100%;
+
+    ul {
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-start;
+      align-items: baseline;
+      list-style: none;
+      margin: 0;
+      padding: 5px;
+    }
+
+    li {
+      color: white;
+    }
+
+    a {
+      margin-left: 10px;
+      color: inherit;
+      text-decoration: none;
+    }
+  `
+
+  const Logo = styled.div`
+    font-size: 20px;
+    color: black;
+    border-radius: 15px;
+    background: #1ed761;
+    padding-right: 12px;
+  `
+
+  return (
+    <Nav>
+      <ul>
+        <li>
+          <Logo>
+            <span role="img" aria-label="handshake">
+              🤝🏼
+            </span>
+            Spotialike
+          </Logo>
+        </li>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+      </ul>
+    </Nav>
+  )
+}
 
 const App = () => {
   return (
-    <div className="app-container">
-      <h1>
-        <span role="img" aria-label="handshake">
-          🤝🏼
-        </span>
-        Spotialike
-      </h1>
-
+    <Container>
       <Router>
-        <nav className="nav">
-          <ul>
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/about">About</NavLink>
-            </li>
-          </ul>
-        </nav>
+        <TopNav />
         <Switch>
           <Route path="/about">
             <About />
@@ -39,7 +86,7 @@ const App = () => {
           </Route>
         </Switch>
       </Router>
-    </div>
+    </Container>
   )
 }
 
--- src/components/About.js
+++ src/components/About.js
@@ -8,10 +8,10 @@ function About() {
         common.
       </p>
       <p>
-        By logging in to your Spotify account you are allowing this app to
-        access your username, used to let find your playlist, and Liked Songs
-        playlist, used to generate an intersections between your and another
-        user's playlist.
+        By logging into your Spotify account you are allowing this app to access
+        your username, used to let find your playlist, and Liked Songs playlist,
+        used to generate an intersections between your and another user's
+        playlist.
       </p>
     </>
   )
